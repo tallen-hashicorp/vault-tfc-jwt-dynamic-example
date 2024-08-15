4813fb977860a8836ff19301a305b2bafc76fa65
Tyler Allen <tyler.allen@hashicorp.com>
2024-08-15 13:04:54 +0100

    testing, not sure what happens

--- tf/main.tf
+++ tf/main.tf
@@ -3,23 +3,24 @@ provider "vault" {
   skip_child_token = true
   address          = var.tfc_vault_dynamic_credentials.default.address
   namespace        = var.tfc_vault_dynamic_credentials.default.namespace
+  
 
   auth_login_token_file {
     filename = var.tfc_vault_dynamic_credentials.default.token_filename
   }
 }
 
-provider "vault" {
-  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-  skip_child_token = true
-  alias            = "ALIAS1"
-  address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
-  namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
+# provider "vault" {
+#   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
+#   skip_child_token = true
+#   alias            = "ALIAS1"
+#   address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
+#   namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
 
-  auth_login_token_file {
-    filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
-  }
-}
+#   auth_login_token_file {
+#     filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
+#   }
+# }
 
 
 variable "tfc_vault_dynamic_credentials" {
