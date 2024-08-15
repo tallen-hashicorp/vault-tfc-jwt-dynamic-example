bf412b10a690ad36f0a6c943edf2523cbada85c0
Tyler Allen <tyler.allen@hashicorp.com>
2024-08-15 13:12:53 +0100

    add this back

--- tf/main.tf
+++ tf/main.tf
@@ -10,17 +10,17 @@ provider "vault" {
   }
 }
 
-# provider "vault" {
-#   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-#   skip_child_token = true
-#   alias            = "ALIAS1"
-#   address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
-#   namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
+provider "vault" {
+  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
+  skip_child_token = true
+  alias            = "ALIAS1"
+  address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
+  namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
 
-#   auth_login_token_file {
-#     filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
-#   }
-# }
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
+  }
+}
 
 
 variable "tfc_vault_dynamic_credentials" {
